79a76d6a9709fcd30e2189cbcd24ae95be1726dc
Giorgadze30 <104830094+Giorgadze30@users.noreply.github.com>
2022-07-09 12:38:16 +0400

    add index.html

---
+++ apps.js
@@ -0,0 +1,16 @@
+//work1
+const mybutton=document.querySelector('#enter');
+function handlebuttonclick (event) {
+    console.log(event);
+    const mybutton=document.querySelector('#enter');
+    mybutton.remove
+}
+mybutton.querySelector('enter', handlemybuttonanter)
+
+//work2
+const body = document.body;
+const myImg = document.createElement('img');
+myImg.src = " https://picsum.photos/id/1/200/300";
+body.append(myImg);
+
+//work3
